640b58dced56b0c9f86410c4acbef4470592523c
Harsh Rawat <harsh.rawat@wisc.edu>
2020-05-03 23:11:21 -0500

    Added the functionality to display both X and Y images

--- src/general_utils.py
+++ src/general_utils.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def generate_sample(loaders):
-    img = next(iter(loaders))[0]
-    print('Shape of Sample Image Tensor is : {}'.format(img.shape))
-    img = np.transpose((img + 1) / 2, (1, 2, 0))
-    plt.imshow(img)
+    loader_X = loaders[0]
+    loader_Y = loaders[1]
+    img_X = next(iter(loader_X))[0]
+    img_Y = next(iter(loader_Y))[0]
+    print('Shape of Sample Image Tensor is : {}'.format(img_X.shape))
+    img_X = np.transpose((img_X + 1) / 2, (1, 2, 0))
+    img_Y = np.transpose((img_Y + 1) / 2, (1, 2, 0))
+    fig, axes = plt.subplots(1, 2)
+    axes[0].imshow(img_X)
+    axes[1].imshow(img_Y)
     plt.show()
